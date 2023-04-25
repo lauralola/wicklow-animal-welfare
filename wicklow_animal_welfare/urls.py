8b1c1b6002ca015b63eb54686e175ed80679a8ee
lauralola <laura6289@gmail.com>
2023-04-25 18:49:49 +0000

    Set up allauth

--- wicklow_animal_welfare/urls.py
+++ wicklow_animal_welfare/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path("", include("homing.urls"), name="homing-urls"),
     path('summernote/',  include('django_summernote.urls'))
 ]
