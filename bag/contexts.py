aa1e43c95c68f9fedad7784259c1454b599661dc
lauralola <laura6289@gmail.com>
2023-05-07 08:47:16 +0000

    Update bag context, add checkout success functionality, to be completed

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,23 +20,19 @@ def bag_contents(request):
             'product': product,
         })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    if total < settings.TAX_BACK_THRESHOLD:
+        tax_back_delta = settings.TAX_BACK_THRESHOLD - total
     else:
-        delivery = 0
-        free_delivery_delta = 0
+        tax_back_delta = 0
     
-    grand_total = delivery + total
+    order_total = total
     
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
+        'tax_back_delta': tax_back_delta,
+        'order_total': order_total,
     }
 
     return context
